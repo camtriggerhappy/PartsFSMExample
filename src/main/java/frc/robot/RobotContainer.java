eac400ea89f9725951df8016a4855d6877813e6b
cameron <ckiw23@gmail.com>
2024-06-24 13:32:18 -0400

    added controller example for how to control the state machine

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,13 +6,24 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Subsytems.Intake;
+import frc.robot.Subsytems.Intake.DesiredAction;
 
 public class RobotContainer {
+  CommandXboxController driverController = new CommandXboxController(0);
+  CommandXboxController operator = new CommandXboxController(1);
+  Intake intake = Intake.getInstance();
+
   public RobotContainer() {
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+
+      operator.a().onTrue(new InstantCommand(() -> intake.setAction(DesiredAction.Intaking)));
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
